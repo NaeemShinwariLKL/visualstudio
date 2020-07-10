ffd97b7bd0b50e49249a448ca6fedbaeca9d66a1
Naeem <naeemullah@hashoofoundation.org>
2020-07-10 11:25:58 +0500

    with property 1

--- WebApplication1/App_Start/Class7.cs
+++ WebApplication1/App_Start/Class7.cs
@@ -8,9 +8,5 @@ namespace WebApplication1.App_Start
     public class Class7
     {
         public int MyProperty1 { get; set; }
-
-        public int MyProperty2 { get; set; }
-        public int MyProperty3 { get; set; }
-        public int MyProperty4 { get; set; }
     }
 }
