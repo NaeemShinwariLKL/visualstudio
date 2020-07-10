9686d535fd038a1e63069e9e20b7d5ba11f52d27
Naeem <naeemullah@hashoofoundation.org>
2020-07-10 23:41:47 +0500

    cc1 revert

--- WebApplication1/App_Start/newfile.cs
+++ WebApplication1/App_Start/newfile.cs
@@ -8,7 +8,5 @@ namespace WebApplication1.App_Start
     public class newfile
     {
         public int cc1 { get; set; }
-        public int cc2 { get; set; }
-        public int cc3 { get; set; }
     }
 }
