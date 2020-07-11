d64e96e45fc315977f385cf0b016df0b580dbab7
Naeem <naeemullah@hashoofoundation.org>
2020-07-11 12:51:02 +0500

    cherry pick

--- WebApplication1/App_Start/newfile.cs
+++ WebApplication1/App_Start/newfile.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.App_Start
 {
     public class newfile
     {
-        public int cc1 { get; set; }
-        public int cc2 { get; set; }
+        public int abc1 { get; set; }
+        public int abc2 { get; set; }
     }
 }
