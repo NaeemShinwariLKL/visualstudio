59bde77b00a3d73985614dc4514bb262564878f0
Naeem <naeemullah@hashoofoundation.org>
2020-07-11 15:29:46 +0500

    add cc4

--- WebApplication1/App_Start/newfile.cs
+++ WebApplication1/App_Start/newfile.cs
@@ -9,6 +9,7 @@ namespace WebApplication1.App_Start
     {
         public int cc1 { get; set; }
         public int cc2 { get; set; }
-        public int cc3 { get; set; }  
+        public int cc3 { get; set; }
+        public int cc4 { get; set; }
     }
 }
