67ba12e487ef9a8a40c3dbd5b61e53a1f7bb31c1
Naeem <naeemullah@hashoofoundation.org>
2020-07-13 09:54:18 +0500

    Rebase Checking

--- WebApplication1/App_Start/Class7.cs
+++ WebApplication1/App_Start/Class7.cs
@@ -10,5 +10,6 @@ namespace WebApplication1.App_Start
         public int MyProperty1 { get; set; }
 
         public int MyProperty2 { get; set; }
+        public int MyPropertyRebaseChecking { get; set; }
     }
 }
